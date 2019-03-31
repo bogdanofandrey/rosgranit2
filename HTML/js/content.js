b16a2477b69188cd7292975527c8f8fbf6e33819
bo-co <ulyanoff@MacBook.local>
2019-03-31 18:33:35 +0300

    [ article ][ update ]

--- HTML/js/content.js
+++ HTML/js/content.js
@@ -1,7 +1,8 @@
-var gallery = null;
+var gallery = null,
+	more = null;
 
 $(document).ready(function() {
-    if ($('div.container > article > div.gallery > div.swiper').length !== 0 && $('div.container > article > div.gallery > div.swiper > ul > li').length > 2 ) {
+    if ($('div.container > article > div.gallery > div.swiper').length !== 0 && $('div.container > article > div.gallery > div.swiper > ul > li').length > 2) {
     	gallery = new Swiper('div.container > article > div.gallery > div.swiper', {
     		loop: true,
     		slidesPerView: 3,
@@ -19,6 +20,28 @@ $(document).ready(function() {
     			prevEl: 'div.container > article > div.gallery > div.prev'
   				}
     		});
-    	}	
+    	}
+    	
+
+    if ($('div.container > article > div.more > div.swiper').length !== 0 && $('div.container > article > div.more > div.swiper > ul > li').length > 2) {
+    	more = new Swiper('div.container > article > div.more > div.swiper', {
+    		loop: true,
+    		slidesPerView: 2,
+    		spaceBetween: 10,
+ 			breakpoints: {
+    			640: {
+      				slidesPerView: 1
+    				},
+    			1040: {
+      				slidesPerView: 2
+    				}
+    			},
+    		navigation: {
+    			nextEl: 'div.container > article > div.more > div.next',
+    			prevEl: 'div.container > article > div.more > div.prev'
+  				}
+    		});
+    	}
+    		
 	return false;
 	});
